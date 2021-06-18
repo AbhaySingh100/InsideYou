a86f54b07d13b473913922de8df7edb7f1f68f52
Abhay Singh <deepakumardev49@gmail.com>
2021-06-18 19:03:40 +0530

    Added Toast

---
+++ app/src/main/java/com/insideyouapp/insideyou/signup.java
@@ -0,0 +1,16 @@
+package com.insideyouapp.insideyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class signup extends AppCompatActivity {
+    //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
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Toast.makeText(getApplicationContext(),"Test",Toast.LENGTH_LONG).show();
+    }
+}
